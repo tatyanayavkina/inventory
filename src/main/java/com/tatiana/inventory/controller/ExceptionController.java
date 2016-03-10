01c1fb5f9fe1ed672c421d89225c2cd4c5143c27
tatyanayavkina <t.yavkina@mail.ru>
2016-03-10 17:27:43 +0300

    обработка ошибок

--- src/main/java/com/tatiana/inventory/controller/ExceptionController.java
+++ src/main/java/com/tatiana/inventory/controller/ExceptionController.java
@@ -1,6 +1,7 @@
 package com.tatiana.inventory.controller;
 
 
+import com.tatiana.inventory.exception.NonDeletableObjectException;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ExceptionController {
-    @ResponseStatus(value= HttpStatus.BAD_REQUEST, reason="No item with requested id")
+    @ResponseStatus(value= HttpStatus.NOT_FOUND, reason="No item with requested id")
     @ExceptionHandler(ObjectNotFoundException.class)
     public void handleObjectNotFoundException() {
 
     }
+
+    @ResponseStatus(value=HttpStatus.METHOD_NOT_ALLOWED, reason="Can not delete item with requested id")
+    @ExceptionHandler(NonDeletableObjectException.class)
+    public void handleNonDeletableObjectException(){}
 }
