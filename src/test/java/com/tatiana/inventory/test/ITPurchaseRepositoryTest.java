6288e6c9f40e24fbb24ddae714934bfd3ca0197d
tatyanayavkina <t.yavkina@mail.ru>
2016-03-13 10:56:11 +0300

    testing example

--- src/test/java/com/tatiana/inventory/test/ITPurchaseRepositoryTest.java
+++ src/test/java/com/tatiana/inventory/test/ITPurchaseRepositoryTest.java
@@ -8,6 +8,12 @@ import com.tatiana.inventory.repository.PurchaseRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
