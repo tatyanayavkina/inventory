96bcd3b16a2164acf76d62fb41f519f7a0599247
tatyanayavkina <t.yavkina@mail.ru>
2016-03-15 14:49:07 +0300

    change test for calculateStartAndEndDate

--- src/test/java/com/tatiana/inventory/entity/SubscriptionTest.java
+++ src/test/java/com/tatiana/inventory/entity/SubscriptionTest.java
@@ -40,12 +40,16 @@ public class SubscriptionTest {
     private void setSubscriptions(){
         // ------
         lastActiveSubscription1 = new Subscription();
-        lastActiveSubscription1.setService( service1 );
-        lastActiveSubscription1.setState( Subscription.ServiceState.ACTIVE );
-        lastActiveSubscription1.setStartDate( getDateFromString( "2016-03-14 15:13:28" ) );
-        lastActiveSubscription1.setEndDate( getDateFromString( "2016-03-15 15:13:28" ) );
+        lastActiveSubscription1.setService(service1);
+        lastActiveSubscription1.setState(Subscription.ServiceState.ACTIVE);
+        lastActiveSubscription1.setStartDate(getDateFromString("2016-03-14 15:13:28"));
+        lastActiveSubscription1.setEndDate(getDateFromString("2016-03-15 15:13:28"));
 
-        lastExpiredSubscription1 = null;
+        lastExpiredSubscription1 = new Subscription();
+        lastExpiredSubscription1.setService( service1 );
+        lastExpiredSubscription1.setState(Subscription.ServiceState.EXPIRED);
+        lastExpiredSubscription1.setStartDate(getDateFromString("2016-03-13 15:13:28"));
+        lastExpiredSubscription1.setEndDate(getDateFromString("2016-03-14 15:13:28"));
         // -----
         lastActiveSubscription2 = null;
 
@@ -67,12 +71,15 @@ public class SubscriptionTest {
     }
 
     @Test
-    public void testCalculateStartAndEndDate(){
+    public void testCalculateStartAndEndDate_ShouldUseEndDateOfLastActive(){
         Subscription subscription1 = new Subscription( service1, "user1@mail.ru" );
         subscription1.calculateStartAndEndDate(lastActiveSubscription1, lastExpiredSubscription1);
         assertEquals(formatter.format(subscription1.getStartDate()), "2016-03-15 15:13:28");
         assertEquals(formatter.format(subscription1.getEndDate()), "2016-03-16 15:13:28");
+    }
 
+    @Test
+    public void testCalculateStartAndEndDate_ShouldUseEndDateOfLastExpired(){
         Subscription subscription2 = new Subscription( service1, "user2@gmail.com" );
         subscription2.calculateStartAndEndDate(lastActiveSubscription2, lastExpiredSubscription2);
         assertEquals(formatter.format(subscription2.getStartDate()), "2016-03-11 15:13:28");
