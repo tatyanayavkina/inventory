41243df641db4a73be07205e46cd51d6934f5425
tatyanayavkina <t.yavkina@mail.ru>
2016-03-15 18:08:46 +0300

    one more test

--- src/test/java/com/tatiana/inventory/controller/SubscriptionControllerTest.java
+++ src/test/java/com/tatiana/inventory/controller/SubscriptionControllerTest.java
@@ -3,6 +3,8 @@ package com.tatiana.inventory.controller;
 import com.tatiana.inventory.Application;
 import com.tatiana.inventory.TestUtil;
 import com.tatiana.inventory.billing.BillingService;
+import com.tatiana.inventory.entity.Service;
+import com.tatiana.inventory.entity.Subscription;
 import com.tatiana.inventory.entry.PurchaseIdentifier;
 import com.tatiana.inventory.repository.ServiceRepository;
 import com.tatiana.inventory.repository.SubscriptionRepository;
@@ -16,7 +18,11 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,5 +57,32 @@ public class SubscriptionControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8))
                 .andExpect(content().string("false"));
+    }
+
+    @Test
+    public void testIsClientHasSubscription_ShouldReturnTrue() throws Exception{
+        Integer serviceId = 5;
+        String clientEmail = "user1@gmail.com";
+        PurchaseIdentifier identifier = new PurchaseIdentifier(serviceId, clientEmail);
+
+        Service service = new Service();
+        service.setId(serviceId);
+        Subscription subscription = new Subscription();
+        subscription.setService(service);
+        subscription.setClient(clientEmail);
+        subscription.setState(Subscription.ServiceState.ACTIVE);
+
+        List<Subscription> subscriptions = new ArrayList<>();
+        subscriptions.add(subscription);
+
+        when(subscriptionRepositoryMock.findByServiceAndClientAndState(serviceId,clientEmail, Subscription.ServiceState.ACTIVE)).thenReturn(subscriptions);
+
+        mockMvc.perform(post("/subscriptions/info")
+                        .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                        .content(TestUtil.convertObjectToJsonBytes(identifier))
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(TestUtil.APPLICATION_JSON_UTF8))
+                .andExpect(content().string("true"));
     }
 }
